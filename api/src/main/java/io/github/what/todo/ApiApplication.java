96dca01087d2a9174ea2226c9db40086a31352a5
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-07-31 20:39:52 +0100

    move ServletInitializer into ApiApplication methods

--- api/src/main/java/io/github/what/todo/ApiApplication.java
+++ api/src/main/java/io/github/what/todo/ApiApplication.java
@@ -1,13 +1,26 @@
 package io.github.what.todo;
 
-import org.springframework.boot.SpringApplication;
+import io.github.what.todo.annotation.Generated;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiApplication {
+@Log4j2
+public class ApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        configureApplication(new SpringApplicationBuilder()).run(args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return configureApplication(builder);
+    }
+
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(ApiApplication.class).bannerMode(Banner.Mode.OFF);
+    }
 }
