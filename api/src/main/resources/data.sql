6fcf14eecbfc03c68470664e27ab39e7e5be459e
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-08-03 23:48:02 +0100

    update insert script

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -5,6 +5,11 @@ INSERT INTO TDTD_USERS (USER_USERNAME,
                         USER_DOB)
 VALUES ('Admin123', 'Admin', 'Admin', 'admin@admin.com', PARSEDATETIME('01/01/1979', 'dd/MM/yyyy'));
 
-INSERT INTO TDTD_TODO (USER_USER_ID,
-                       TODO_TASK)
-VALUES ((SELECT user_id FROM tdtd_users WHERE user_username = 'Admin123'), 'Sample Task');
+INSERT INTO TDTD_TODO (USER_ID, TODO_TASK, TODO_IS_COMPLETED)
+VALUES ((SELECT user_id FROM tdtd_users WHERE user_username = 'Admin123'), 'Sample Task 1', TRUE),
+       ((SELECT user_id FROM tdtd_users WHERE user_username = 'Admin123'), 'Sample Task 2', FALSE),
+       ((SELECT user_id FROM tdtd_users WHERE user_username = 'Admin123'), 'Sample Task 3', FALSE),
+       ((SELECT user_id FROM tdtd_users WHERE user_username = 'Admin123'), 'Sample Task 4', TRUE),
+       ((SELECT user_id FROM tdtd_users WHERE user_username = 'Admin123'), 'Sample Task 5', FALSE)
+
+;
