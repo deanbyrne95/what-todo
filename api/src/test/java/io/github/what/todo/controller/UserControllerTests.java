a634de99edaa9bba646aef81c6820e4835cf83d6
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-08-05 10:08:52 +0100

    update UserControllerTests

--- api/src/test/java/io/github/what/todo/controller/UserControllerTests.java
+++ api/src/test/java/io/github/what/todo/controller/UserControllerTests.java
@@ -1,28 +1,50 @@
 package io.github.what.todo.controller;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import io.github.what.todo.entity.User;
+import io.github.what.todo.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.sql.Date;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
 public class UserControllerTests {
 
-    @Mock
-    private UserController userController;
+    private final String baseUrl = "/api/v1/user";
 
-    @BeforeEach
-    public void init() {
-        userController = new UserController();
-        MockitoAnnotations.openMocks(userController);
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getUserById() throws Exception {
+        User mockUser = new User("Test123", "Test", "123", "Test123@test.com", new Date(12345L));
+
+        when(userService.getUserById(any(Long.class))).thenReturn(mockUser);
+
+        mockMvc.perform(get(baseUrl + "/{id}", 1L)
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void getUser() {
-        ResponseEntity re = userController.getUser();
-        assertEquals("Hello World!", re.getBody());
+    void saveUser() throws Exception {
+        User mockUser = new User(1L,"Test123", "Test", "123", "Test123@test.com", new Date(12345L));
+
+        doNothing().when(userService).saveUser(any(User.class));
+
+        mockMvc.perform(post(baseUrl + "/save", mockUser)).andReturn();
     }
 }
